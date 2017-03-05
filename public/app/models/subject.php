947ddcbd8e55bcff222d3305991e376582a93c30
cbalamanatuic@gmail.com <clydeinwebdev@gmail.com>
2017-03-06 02:23:45 +0800

    fixed take exam page

--- public/app/models/subject.php
+++ public/app/models/subject.php
@@ -34,12 +34,6 @@
 			else if($_POST['action']=="deletesubject"){
 				echo "delete subject ok!";
 				$id = $_POST['id'];
-				// $name = $_POST['name'];
-				// $description = $_POST['description'];
-				// $timeduration = $_POST['timeduration'];
-				// $passingrate = $_POST['passingrate'];
-				// $attempt = $_POST['attempt'];
-				// $items = $_POST['items'];
 				$sql = "delete from $table where id='$id'";
 				$result = mysqli_query($link, $sql) or die("Invalid query" . mysqli_error($link));
 				echo "ok";
@@ -55,26 +49,12 @@
 					$result2 = mysqli_query($link, $sql2) or die("Invalid query" . mysqli_error($link));
 					$topic = array();
 					while($row2=mysqli_fetch_assoc($result2)){
-						// $data = array("id" => $row[, "name" => "test");
-						// $topic[] = $row2['name'];
-						// $test = array("id" => 1, "name" => "test");
 						$topic[] = array("id" => $row2['id'], "name" => htmlspecialchars($row2['name']));
 					}
-					// print_r($topic);
-					// array_push($row, $topic);
-					// // array_push($topic, "criminology", "legal");
-					// if(count($topic)>0)
-					// $test = array();
-					
 					array_push($row, $topic);					
 					$arr[] = $row;
-					// print_r($arr);
-					// echo "-------------";
-					// if($i++==1)break;
 				}
 				echo json_encode($arr);
-				// echo $arr;
-				// print_r($arr);
 			}
 		}
 		else{	
